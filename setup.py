fbe6cab1dd2589f3af38fed0540f8c1d584ce8de
Qiusheng Wu <giswqs@gmail.com>
2021-01-24 10:23:08 -0500

    Bump version: 0.1.7 → 0.1.8

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/whiteboxgui',
-    version='0.1.7',
+    version='0.1.8',
     zip_safe=False,
 )
