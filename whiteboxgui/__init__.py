62f8f6b0834618ab5e6dd582425d84a97cba9ef7
Qiusheng Wu <giswqs@gmail.com>
2023-03-28 22:00:07 -0400

    Add support for Google Colab

--- whiteboxgui/__init__.py
+++ whiteboxgui/__init__.py
@@ -4,4 +4,10 @@ __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
 __version__ = '2.2.0'
 
+import sys
+
 from .whiteboxgui import show, update_package
+
+if "google.colab" in sys.modules:
+    from google.colab import output
+    output.enable_custom_widget_manager()
